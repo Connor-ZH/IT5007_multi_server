34e1c0d1da83fb722f9cf029707891e75e3234ce
Connor-ZH <zhang_heng@u.nus.edu>
2021-10-27 14:45:01 +0000

    update

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -279,13 +279,10 @@ async function graphQLFetch(query, variables = {}) {
       if (error.extensions.code == 'BAD_USER_INPUT') {
         const details = error.extensions.exception.errors.join('\n ');
         alert(`${error.message}:\n ${details}`);
-      } else {
-        alert(`${error.extensions.code}: ${error.message}`);
       }
     }
     return result.data;
   } catch (e) {
-    alert(`Error in sending data to server: ${e.message}`);
   }
 }
 
